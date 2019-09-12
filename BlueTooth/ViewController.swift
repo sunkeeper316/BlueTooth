14d5ac6ef8a469a39a376b521c9828c9502e992d
sun_huang <sun_huang@charder.com.tw>
2019-09-12 13:03:21 +0800

    Initial Commit

---
+++ BlueTooth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueTooth
+//
+//  Created by 黃德桑 on 2019/9/12.
+//  Copyright © 2019 黃德桑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
